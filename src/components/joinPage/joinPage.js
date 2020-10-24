531c52bd5752c0e978709cd23a1c39de8b46f2fb
Devin <devinxclemons@gmail.com>
2020-10-24 19:03:43 -0400

    Create List

--- src/components/joinPage/joinPage.js
+++ src/components/joinPage/joinPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './joinPage.css';
 
 function JoinPage(){
     return(
         <div>
+                <button className="createList" onclick="Create List "> Create List </button>
                 
+                <form>
+                    <label>
+                        Enter List Code:
+                        <input type="text" name = "Enter Code"/>
+                    </label>
+                    
+                </form>
         </div>
     )
 }
+
+
 
 export default JoinPage;
